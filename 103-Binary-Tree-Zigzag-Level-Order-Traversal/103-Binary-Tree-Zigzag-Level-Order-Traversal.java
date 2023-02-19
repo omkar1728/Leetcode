94500532a26c2eedab0c32905692c07dd65c44a2
Omkar <omkar.mahajan872@gmail.com>
2023-02-19 09:35:42 +0530

    '103-Binary-Tree-Zigzag-Level-Order-Traversal'

---
+++ 103-Binary-Tree-Zigzag-Level-Order-Traversal/103-Binary-Tree-Zigzag-Level-Order-Traversal.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<Integer,ArrayList<Integer>> map = new HashMap<Integer,ArrayList<Integer>>();
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> zig = new ArrayList<>();
+        if(root == null) return zig;
+        bfs(root, 0);
+        for(Map.Entry<Integer,ArrayList<Integer>> mapElement : map.entrySet()){
+            if(mapElement.getKey()%2 == 0){
+                zig.add(mapElement.getValue());
+            } else {
+                Collections.reverse(mapElement.getValue());
+                zig.add(mapElement.getValue());
+            }
+        }
+        return zig;
+    }
+    private void bfs(TreeNode root, int level){
+        if (root == null) return;
+        if(map.containsKey(level)){
+            map.get(level).add(root.val);
+        } else {
+            ArrayList<Integer> temp = new ArrayList<Integer>();
+            temp.add(root.val);
+            map.put(level, temp );
+        }
+        bfs(root.left, level + 1);
+        bfs(root.right, level + 1);
+    }
+}
---
+++ 103-Binary-Tree-Zigzag-Level-Order-Traversal/README.md
@@ -0,0 +1,13 @@
+103-Binary-Tree-Zigzag-Level-Order-Traversal
+
+
+Java
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/submissions/900735719/
+Runtime
+1 ms
+Beats
+87.53%
+Memory
+41.4 MB
+Beats
+24.54%
