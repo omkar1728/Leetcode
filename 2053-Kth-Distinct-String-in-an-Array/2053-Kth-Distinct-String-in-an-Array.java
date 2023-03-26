ff8cdf254ef313a7b1733a62044f881b37104663
Omkar <omkar.mahajan872@gmail.com>
2023-03-26 20:01:09 +0530

    '2053-Kth-Distinct-String-in-an-Array'

---
+++ 2053-Kth-Distinct-String-in-an-Array/2053-Kth-Distinct-String-in-an-Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String,Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(map.get(arr[i]) == 1){
+                k--;
+            }
+            if(k == 0) return arr[i];
+        }
+        return "";
+    }
+}
---
+++ 2053-Kth-Distinct-String-in-an-Array/README.md
@@ -0,0 +1,13 @@
+2053-Kth-Distinct-String-in-an-Array
+
+
+Java
+https://leetcode.com/problems/kth-distinct-string-in-an-array/submissions/922475373/
+Runtime
+6 ms
+Beats
+78.10%
+Memory
+42.5 MB
+Beats
+27.62%
