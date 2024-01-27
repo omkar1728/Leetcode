8410b3005f66f9cd2a2c704a2cb93c3ab12e6b88
omkar1728 <90204934+omkar1728@users.noreply.github.com>
2024-01-27 11:36:30 +0530

    Time: 601 ms (10.28%), Space: 30.7 MB (99.40%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        ans = 0
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        
+        for i in d.keys():
+            if d[i] == 1:
+                return -1
+            elif d[i]%3 == 1:
+                ans += d[i]//3 + 1
+            elif d[i]%3 == 2:
+                ans += d[i]//3 + 1
+            else:
+                ans += d[i]//3
+
+
+
+        return ans
